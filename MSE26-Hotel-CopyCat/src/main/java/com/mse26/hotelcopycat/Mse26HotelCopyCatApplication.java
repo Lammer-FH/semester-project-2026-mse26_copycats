9258954f06352a1acde59ea1f89d246744acd412
LilboX3 <lilligruber02@gmail.com>
2026-05-19 18:48:56 +0200

    Springboot project initialization

---
+++ MSE26-Hotel-CopyCat/src/main/java/com/mse26/hotelcopycat/Mse26HotelCopyCatApplication.java
@@ -0,0 +1,13 @@
+package com.mse26.hotelcopycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mse26HotelCopyCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mse26HotelCopyCatApplication.class, args);
+	}
+
+}
---
+++ MSE26-Hotel-CopyCat/src/test/java/com/mse26/hotelcopycat/Mse26HotelCopyCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mse26.hotelcopycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mse26HotelCopyCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
