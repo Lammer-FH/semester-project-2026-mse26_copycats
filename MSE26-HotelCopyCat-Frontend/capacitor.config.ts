aa763f13935b9047a2dc869b380cb30fee27cf9f
LilboX3 <lilligruber02@gmail.com>
2026-05-26 10:38:12 +0200

    switched frontend with ionic vue project

---
+++ MSE26-HotelCopyCat-Frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MSE26-HotelCopyCat-Frontend',
+  webDir: 'dist'
+};
+
+export default config;
